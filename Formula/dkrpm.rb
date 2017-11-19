a9581cece59b4265c207f38d4a2e6757097784de
Gleb Goncharov <inbox@gongled.ru>
2017-11-19 16:36:57 +0400

    Bump to 1.4.0

--- Formula/dkrpm.rb
+++ Formula/dkrpm.rb
@@ -1,8 +1,8 @@
 class Dkrpm < Formula
   desc 'Build RPM packages using Docker containers'
   homepage 'https://github.com/gongled/dkrpm'
-  url 'https://github.com/gongled/dkrpm/archive/v1.3.1.tar.gz'
-  version '1.3.1'
+  url 'https://github.com/gongled/dkrpm/archive/v1.4.0.tar.gz'
+  version '1.4.0'
   head 'https://github.com/gongled/dkrpm'
 
   depends_on 'docker' => [:build, :run]
