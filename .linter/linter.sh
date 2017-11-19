e7b33ce4d77a2fa7d01fe0d9521b3cee690880d5
Gleb Goncharov <inbox@gongled.ru>
2017-11-19 19:21:31 +0300

    Fix linter test

--- .linter/linter.sh
+++ .linter/linter.sh
@@ -6,4 +6,10 @@ export SHELLCHECK_OPTS="-e SC2059 -e SC2034 -e SC1090 -e SC2154"
 # Run linter
 if type shellcheck &>/dev/null ; then
     shellcheck "$@"
+else
+    echo
+    echo "Shellcheck is not installed or cannot be found at \$PATH."
+    echo
+    echo "Follow the installation instructions on the official site to resolve this issue:"
+    echo "<https://github.com/koalaman/shellcheck#installing>"
 fi
